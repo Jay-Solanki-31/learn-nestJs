f874d26e98e7c9a8b86604607dd726cd88d60205
Jay Solanki <jaysolanki1733@gmail.com>
2025-06-19 13:49:52 +0530

    add .env  and learn how to get data

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
 import { CatsModule } from './cats/cats.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HelloModule, CatsModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+        isGlobal:true
+      }),HelloModule, CatsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+
+  constructor(private configService:ConfigService){}
   getHello(): string {
+
+    const appname = this.configService.get<string>('APP_NAME', 'defaultvalue')
+    console.log('app name is ', appname);
     return 'Hello World!';
   }
 }
